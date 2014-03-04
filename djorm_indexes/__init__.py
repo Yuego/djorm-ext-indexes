c1e163e3c074baf484e00195e347386318c77858
Yuego [Lenovo] <root@proscript.ru>
2014-03-04 19:21:09 +0400

    fix

--- djorm_indexes/__init__.py
+++ djorm_indexes/__init__.py
@@ -25,16 +25,14 @@ if not hasattr(DatabaseCreation, '_sql_indexes_for_field'):
         qn = self.connection.ops.quote_name
         db_table = model._meta.db_table
 
-        if isinstance(f, pg_fields):
-            if f.db_index:
-
-                output = [(style.SQL_KEYWORD('CREATE INDEX ') +
-                              style.SQL_TABLE(qn('%s_%s_gin' % (db_table, f.column))) +
-                              style.SQL_KEYWORD(' ON ') +
-                              style.SQL_TABLE(qn(db_table)) +
-                              style.SQL_KEYWORD(' USING ') +
-                              style.SQL_COLTYPE('GIN') + ' ( ' +
-                              style.SQL_FIELD(qn(f.column)) + ' );')]
+        if isinstance(f, pg_fields) and f.db_index:
+            output = [(style.SQL_KEYWORD('CREATE INDEX ') +
+                       style.SQL_TABLE(qn('%s_%s_gin' % (db_table, f.column))) +
+                       style.SQL_KEYWORD(' ON ') +
+                       style.SQL_TABLE(qn(db_table)) +
+                       style.SQL_KEYWORD(' USING ') +
+                       style.SQL_COLTYPE('GIN') + ' ( ' +
+                       style.SQL_FIELD(qn(f.column)) + ' );')]
         else:
             output = self._sql_indexes_for_field(model, f, style)
 
